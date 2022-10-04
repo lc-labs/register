40fc592727cee66a5cf0923b3a42a56847632e6c
wistec-bisrum <erik.davis@reserve.org>
2022-10-04 16:59:43 -0600

    fix spacing/styling of about issuance box

--- src/views/issuance/components/about/index.tsx
+++ src/views/issuance/components/about/index.tsx
@@ -10,26 +10,28 @@ const About = () => (
     }}
     p={4}
   >
-    <Text sx={{ fontWeight: 500 }}>
-      <Trans>About this app</Trans>
-    </Text>
-    <br /> <br />
-    <Text>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Senectus et netus et
-      malesuada fames ac turpis egestas integer. Ac turpis egestas integer eget
-      et pharetra pharetra massa massa. Nibh venenatis cras sed felis. Ante
-      metus dictum at tempor commodo ullamcorper.
+    <Text sx={{ fontWeight: 500, display: 'block' }} mb={2}>
+          <Trans>Mint/Redeem</Trans>
+      </Text>
+      <Text sx={{ color: 'lightText' }}>
+        Ac turpis egestas maecenas pharetra. Habitant morbi tristique senectus
+        et. Dapibus ultrices in iaculis nunc sed augue lacus viverra vitae.
+        Bibendum est ultricies integer quis auctor. Nunc congue nisi vitae
+        suscipit tellus mauris. ollicitudin. Nulla facilisi etiam dignissim diam
+        quis enim lobortis.
+      </Text>
       <br />
       <br />
-      <Text sx={{ fontWeight: 500 }}>Mint/Redeem</Text>
-      <br /> <br />
-      Ac turpis egestas maecenas pharetra. Habitant morbi tristique senectus et.
-      Dapibus ultrices in iaculis nunc sed augue lacus viverra vitae. Bibendum
-      est ultricies integer quis auctor. Nunc congue nisi vitae suscipit tellus
-      mauris. ollicitudin. Nulla facilisi etiam dignissim diam quis enim
-      lobortis.
-    </Text>
+      <Text sx={{ fontWeight: 500, display: 'block' }} mb={2}>
+        <Trans>About this app</Trans>
+      </Text>
+      <Text sx={{ color: 'lightText' }}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Senectus et netus et
+        malesuada fames ac turpis egestas integer. Ac turpis egestas integer
+        eget et pharetra pharetra massa massa. Nibh venenatis cras sed felis.
+        Ante metus dictum at tempor commodo ullamcorper.
+      </Text>
   </Box>
 )
 
